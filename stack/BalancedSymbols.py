434f9a72406c5ef8837f1125bc5beaf0c44668c2
BestCoderForever <ali_irturk@berkeley.edu>
2018-11-06 14:32:01 -0800

    this is for the linked list data structure related files

--- stack/BalancedSymbols.py
+++ stack/BalancedSymbols.py
@@ -25,17 +25,17 @@ for symbol in symbols:
         if not(s.isEmpty()):
             topStack = s.pop()
         else:
-            print "Unbalanced because the stack is empty"
+            print("Unbalanced because the stack is empty")
             isBalanced = False
             break
         if matchingDict[symbol] != topStack:
-            print "Unbalanced"
+            print("Unbalanced")
             isBalanced = False
             break
     else:
-        print "Some unexpected text was entered!"
+        print("Some unexpected text was entered!")
 
-print ('The balance of the system is %r' % isBalanced)
+print('The balance of the system is %r' % isBalanced)
 
 
 
