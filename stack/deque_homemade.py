1586a3c883b7dd159da5d15202226f9fa5475004
BestCoderForever <ali_irturk@berkeley.edu>
2018-11-06 14:35:08 -0800

    this is for the linked list data structure related files

---
+++ stack/deque_homemade.py
@@ -0,0 +1,53 @@
+__author__ = 'airturk'
+
+# Definition of the abstract data type
+
+# Dequeu() -> creates a empty deque
+# add_front(item) -> allows user to add an item to the front of the deque
+# remove_from -> allows user to remove an item from the front of the deque, and return it
+# add_back(item) -> allows user to add an item to the back of the deque
+# remove_back() -> allows user to remove an item from the back of the deque
+# isEmpty() -> returns True if the deque is empty, false otherwise
+# size() -> returns the size of the deque
+
+
+class Deque:
+
+    def __init__(self):
+        self.dq = [] # create an empty list
+
+    def add_front(self, item):
+        self.dq.append(item)
+
+    def remove_front(self):
+        self.dq.pop()
+
+    def add_back(self, item):
+        self.dq.insert(0, item)
+
+    def remove_back(self):
+        self.dq.pop(0)
+
+    def isEmpty(self):
+        print(self.dq == [])
+
+    def size(self):
+        print(len(self.dq))
+        return len(self.dq)
+
+#dq = Deque()
+#dq.isEmpty()
+#dq.size()
+
+#dq.add_front('100')
+#dq.add_front('101')
+#dq.add_front('102')
+#dq.add_back('200')
+#dq.add_back('201')
+#dq.add_back('202')
+
+#dq.remove_back()
+#dq.remove_front()
+
+#dq.remove_back()
+#dq.remove_front()
---
+++ stack/queue_homemade.py
@@ -0,0 +1,61 @@
+__author__ = 'airturk'
+
+# step 1 is to define the abstract data type for queue
+
+# Queue() -> create a new queue that is empty, returns the queue
+# enqueue(item) -> add item in the queue, returns nothing
+# dequeue() -> remove an item from the queue, return the item
+# isEmpty() -> check to see if the queue is empty, print True if it is empty, and False if not empty
+# size() -> print the size of the queue
+
+class QueueNew():
+
+    def __init__(self):
+        self.q = [] # create an empty list
+
+    def enqueue(self, item):
+        # add the item in the queue
+        self.q.append(item)
+
+    def dequeue(self):
+        # remove the item from the queue
+        return self.q.pop(0)
+
+    def isEmpty(self):
+        print(self.q == [])
+
+    def size(self):
+        print(len(self.q))
+        return len(self.q)
+
+    def peek_elements(self):
+        return self.q
+
+# Test code starts here
+#s = QueueNew()
+
+#s.isEmpty()
+
+#s.size()
+
+#s.enqueue('ALI')
+
+#s.enqueue('VELI')
+
+#s.enqueue("49")
+
+#s.enqueue("50")
+
+#print(s.dequeue())
+
+#print(s.dequeue())
+
+#s.isEmpty()
+
+#s.size()
+
+#print(s.dequeue())
+
+#s.isEmpty()
+
+#s.size()
